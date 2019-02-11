1fb55f12ca43978da3d1bf893405d4c537f2b771
Isabelle Baert <webrelationpro@gmail.com>
2019-02-11 15:26:43 +0100

    questions ask at Stupid Coaching

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'ask' => 'questions#ask'
+  get 'answer' => 'questions#answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
